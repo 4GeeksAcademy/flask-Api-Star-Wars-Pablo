e68ee0e3bddab3ad2805c8fdee19e502745f3b3b
PabloPh95 <pablowork95@gmail.com>
2025-05-29 10:46:07 +0000

    Models ok

--- src/app.py
+++ src/app.py
@@ -38,12 +38,47 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users= db.session.query(User).all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    if users is None:
+        return jsonify({"error": "users not found"}), 404
 
-    return jsonify(response_body), 200
+
+    return jsonify([user.serialize() for user in users]), 200
+
+##Renderizado de Usuarios##
+
+@app.route('/user/<int:id>', methods=['GET'])
+def getUserById(id):
+    user= User.query.filter_by(id=id).first() ##Primer ID es del "models" y el segundo es el de la consulta (la url)
+    if not user:
+        return jsonify({"error": "user not found"}), 400
+    return jsonify({"user": user.serialize()}), 200
+
+##Creación de usuarios##
+
+@app.route('/user', methods=['POST'])
+def newUser():
+    request_body=request.get_json()
+    email = request_body["email"]
+    password = request_body["password"]
+    if not email or not password:
+        return jsonify ({"msg": "Todos los campos son requeridos"}), 400
+    exist = User.query.filter_by(email=email).first()
+    if exist:
+        return jsonify({"msg": "Correo ya utilizado"}), 400
+    new_user = User(email= email, password=password, is_active= True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "Usuario creado"}), 201
+
+##Agregar Character Favorito a Usuarios##
+
+##Agregar Planet Favorito a Usuarios##
+
+##Eliminar Character Favoritos de Usuarios##
+
+##Eliminar Planet Favoritos de Usuarios##
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,90 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__="user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorite_characters: Mapped[list["FavoriteCharacter"]] = relationship(back_populates="user")
+    favorite_planets: Mapped[list["FavoritePlanet"]] = relationship(back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorite_characters": [fav.serialize() for fav in self.favorite_characters],
+            "favorite_planets": [fav.serialize() for fav in self.favorite_planets]
+        }
+
+class Character(db.Model):
+    __tablename__="character"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    hair_color: Mapped[str] = mapped_column(nullable=False)
+    eye_color: Mapped[str] = mapped_column(nullable=False)
+
+    favorited_by: Mapped[list["FavoriteCharacter"]] = relationship(back_populates="character")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+        }
+    
+
+class FavoriteCharacter(db.Model):
+    __tablename__="favorite_character"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    character_id: Mapped[int] = mapped_column(ForeignKey("character.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="favorite_characters")
+    character: Mapped["Character"] = relationship(back_populates="favorited_by")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character": self.character.serialize()
+        }
+    
+
+class Planet(db.Model):
+    __tablename__="planet"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    diameter: Mapped[str] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
+    
+    favorite_planets: Mapped[list["FavoritePlanet"]] = relationship(back_populates="planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "terrain": self.terrain,
+        }
+    
+
+class FavoritePlanet(db.Model):
+    __tablename__="favorite_planet"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="favorite_planets")
+    planet: Mapped["Planet"] = relationship(back_populates="favorite_planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet": self.planet.serialize()
         }
